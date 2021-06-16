883841b6c9519324b5f361e9f94c7b8f207e1fad
Victor Isidoro <victorisidoro@icloud.com>
2021-06-16 16:05:55 +0100

    Create index.js and print 4 colourful names

---
+++ index.js
@@ -0,0 +1,7 @@
+// Colorfull Wilders
+const chalk = require("chalk")
+
+console.log(chalk.red("Alexej"))
+console.log(chalk.green("Guilherme"))
+console.log(chalk.yellow("Lorianne"))
+console.log(chalk.blue("Natalie"))
